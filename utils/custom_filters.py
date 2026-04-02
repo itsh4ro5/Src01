f4e23ca87dee16eab3c67e6fccf62c735b573493
jai sharma <jai1787@users.noreply.huggingface.co>
2026-04-02 20:53:18 +0000

    Update utils/custom_filters.py

--- utils/custom_filters.py
+++ utils/custom_filters.py
@@ -7,6 +7,9 @@ from pyrogram import filters
 user_steps = {}
 
 def login_filter_func(_, __, message):
+    # Sabse pehle check karo ki from_user exist karta bhi hai ya nahi
+    if not message.from_user:
+        return False
     user_id = message.from_user.id
     return user_id in user_steps
 
