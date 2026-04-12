bba22de5b84d74c656d7efd63fbb80f1390015ef
jai sharma <jai1787@users.noreply.huggingface.co>
2026-04-12 16:11:59 +0000

    Update plugins/premium.py

--- plugins/premium.py
+++ plugins/premium.py
@@ -84,10 +84,10 @@ async def start_handler(client, message):
     b9 = spy.b64decode(a10).decode()
     b10 = spy.b64decode(a11).decode()
 
-    tm = await getattr(app, b3)(b1, b2)
+    #tm = await getattr(app, b3)(b1, b2)
 
-    pb = getattr(tm, spy.b64decode(attr1.encode()).decode())
-    fd = getattr(pb, spy.b64decode(attr2.encode()).decode())
+    #pb = getattr(tm, spy.b64decode(attr1.encode()).decode())
+    #fd = getattr(pb, spy.b64decode(attr2.encode()).decode())
 
     kb = IKM([
         [IK(b7, url=JL)],
@@ -95,7 +95,7 @@ async def start_handler(client, message):
     ])
 
     await getattr(message, b4)(
-        fd,
+        "https://i.ibb.co/qLSbQYGP/x.jpg",
         caption=b6,
         reply_markup=kb
     )
