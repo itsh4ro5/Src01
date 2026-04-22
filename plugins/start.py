1c61f5a9927298b8529519b9786b18deb60cd5b1
jai sharma <jai1787@users.noreply.huggingface.co>
2026-04-22 03:52:02 +0000

    Update plugins/start.py

--- plugins/start.py
+++ plugins/start.py
@@ -2,7 +2,7 @@ from shared_client import app
 from pyrogram import filters
 from pyrogram.errors import UserNotParticipant
 from pyrogram.types import BotCommand, InlineKeyboardButton, InlineKeyboardMarkup
-from config import, OWNER_ID, FORCE_SUB
+from config import OWNER_ID, FORCE_SUB
 
 async def subscribe(app, message):
     if FORCE_SUB:
